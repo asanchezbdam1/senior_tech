813052e925a325137d9df6fc5641bdeb0efe0c53
Asier <asanchezbdam1@gmail.com>
2022-04-11 20:52:49 +0200

    Añadido widget personalizado desplegable.

--- lib/applist.dart
+++ lib/applist.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:senior_tech/my-widgets.dart';
 
 class AppList extends StatelessWidget {
+  const AppList({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +26,44 @@ class AppList extends StatelessWidget {
         title: const Text("Cómo usar el móvil"),
       ),
       backgroundColor: const Color(0xFF9A2828),
+      body: Center(
+          child: Column(
+        children: [
+          const ExpandableContainer(
+            title: Text("Título"),
+            child: Text("Hijo"),
+            backgroundColor: Colors.red,
+            collapsedBackgroundColor: Colors.blue,
+          ),
+          const ExpandableContainer(
+            radius: 20,
+            gradient: LinearGradient(
+              colors: [Colors.green, Colors.red],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            title: Text("Título 2"),
+            child: Text("Hijo 2"),
+            backgroundColor: Colors.transparent,
+            collapsedBackgroundColor: Colors.transparent,
+          ),
+          ExpandableContainer(
+            title: Container(
+              child: const Text("Título"),
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.black, Colors.red],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+            ),
+            child: const Text("Hijo 3"),
+            backgroundColor: Colors.blue,
+            collapsedBackgroundColor: Colors.green,
+          ),
+        ],
+      )),
     );
   }
 }
--- lib/my-widgets.dart
+++ lib/my-widgets.dart
@@ -40,3 +40,59 @@ class GradButton extends StatelessWidget {
     );
   }
 }
+
+class ExpandableContainer extends StatefulWidget {
+  final Widget title;
+  final Gradient? gradient;
+  final Color? backgroundColor;
+  final Color? collapsedBackgroundColor;
+  final double? radius;
+  final double? top;
+  final double? bottom;
+  final double? right;
+  final double? left;
+  final Widget child;
+
+  const ExpandableContainer({
+    Key? key,
+    required this.title,
+    required this.child,
+    this.radius,
+    this.backgroundColor,
+    this.collapsedBackgroundColor,
+    this.gradient,
+    this.top,
+    this.bottom,
+    this.right,
+    this.left,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ExpandableContainerState();
+}
+
+class _ExpandableContainerState extends State<ExpandableContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+          padding: EdgeInsets.only(
+            left: widget.left ?? 0,
+            top: widget.top ?? 0,
+            right: widget.right ?? 0,
+            bottom: widget.bottom ?? 0,
+          ),
+          decoration: BoxDecoration(
+            gradient: widget.gradient,
+            borderRadius: BorderRadius.circular(widget.radius ?? 0),
+          ),
+          child: ExpansionTile(
+            title: widget.title,
+            backgroundColor: widget.backgroundColor ?? Colors.transparent,
+            collapsedBackgroundColor:
+                widget.collapsedBackgroundColor ?? Colors.transparent,
+            children: [widget.child],
+          )),
+    );
+  }
+}
